ad5fc3cae62afd56d10371e58fc9e245a428eb17
Akram180897 <imakramsheikh@gmail.com>
2026-09-17 11:58:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste ONLY your Supabase project URL and PUBLISHABLE (anon) key here.
 // Never put a Supabase service_role/secret key in this file.
-const SUPABASE_URL = "dcglkmwzogmmoatcjkdv";
+const SUPABASE_URL = "sb_publishable_ebUPBhN70-XnRLAREMnTBQ_5xoIc_4K";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ebUPBhN70-XnRLAREMnTBQ_5xoIc_4K";
